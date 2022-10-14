0da7df6de7c7be29221c493e089a33944d247080
Boris D <boris.dob@gmail.com>
2022-10-14 10:34:55 -0700

    Add ocaml-ios 5.0.0~beta1 cross toolchain.

---
+++ packages/ocaml-ios64.5.0.0~beta1/files/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+HOST=$1
+
+./configure --host=$1
+
+make runtime/primitives runtime/sak SAK_CC=cc SAK_LINK='cc -o $(1) $(2)'
+
+cp `which ocamlrun` runtime/ocamlrun
+cp -f Makefile.cross Makefile.config
+cp -f s-ios.h runtime/caml/s.h
+cp -f m-ios.h runtime/caml/m.h
+
+make world opt \
+  compilerlibs/ocamlcommon.cmxa compilerlibs/ocamlbytecomp.cmxa \
+  compilerlibs/ocamloptcomp.cmxa driver/main.cmx driver/optmain.cmx \
+  runtime_PROGRAMS= \
+  OCAMLRUN=ocamlrun \
+  NEW_OCAMLRUN=ocamlrun
